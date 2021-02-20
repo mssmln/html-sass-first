a0ffedf4c3081d33cfa3333763f800802aa4b0c7
Massimiliano <m.croci.ly@gmail.com>
2021-02-20 19:02:41 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,3 @@
+// Realizzate un semplice layout come tanti altri fatti fino ad ora con un header, 3 colonne e un footer, tutto utilizzando alcune regole base di sass e ovviamente laravel mix.
+// Bonus: aggiungete a piacere altre sezioni.
+// Ricordare sempre di aggiungere .gitignore per ignorare node_modules.
